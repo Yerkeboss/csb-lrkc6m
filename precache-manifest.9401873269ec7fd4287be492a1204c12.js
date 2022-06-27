2e67ad3d846d6ef285df6167d223b34f991ac69a
Code Sandbox <hello@codesandbox.io>
2022-06-27 12:37:54 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9401873269ec7fd4287be492a1204c12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437d3249822f36831b7307ef6e18a881",
+    "url": "https://yerkeboss.github.io/csb-lrkc6m/index.html"
+  },
+  {
+    "revision": "ce7c0c123aafb732275c",
+    "url": "https://yerkeboss.github.io/csb-lrkc6m/static/css/main.4d681f17.chunk.css"
+  },
+  {
+    "revision": "75014c2f41e60c455dd0",
+    "url": "https://yerkeboss.github.io/csb-lrkc6m/static/js/2.64ce3e63.chunk.js"
+  },
+  {
+    "revision": "68481d214dfda62d485a39c2c450691e",
+    "url": "https://yerkeboss.github.io/csb-lrkc6m/static/js/2.64ce3e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7c0c123aafb732275c",
+    "url": "https://yerkeboss.github.io/csb-lrkc6m/static/js/main.154f17df.chunk.js"
+  },
+  {
+    "revision": "e1c613e2551c43126331",
+    "url": "https://yerkeboss.github.io/csb-lrkc6m/static/js/runtime-main.8264c116.js"
+  }
+]);
